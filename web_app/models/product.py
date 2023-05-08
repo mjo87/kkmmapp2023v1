52e082d11a6c42baf40ce7062d365dec004cd869
mjo87 <128439833+mjo87@users.noreply.github.com>
2023-05-08 11:53:58 -0400

    troubleshooting products

--- web_app/models/product.py
+++ web_app/models/product.py
@@ -4,7 +4,7 @@ DEFAULT_PRODUCTS = [
     {'id': 1, 'name': 'Hoodie', 'description': 'For Men', 'price': 44.99, 'url': 'https://images.footballfanatics.com/georgetown-hoyas/mens-champion-navy-georgetown-hoyas-team-arch-reverse-weave-pullover-hoodie_pi4266000_altimages_ff_4266580-8b5a060e842e7a7316b9alt1_full.jpg?_hv=2&w=900'},
     {'id': 2, 'name': 'Sweater', 'description': 'For Women', 'price': 39.99, 'url': 'https://images.footballfanatics.com/georgetown-hoyas/womens-cutter-and-buck-navy-georgetown-hoyas-lakemont-tri-blend-v-neck-pullover-sweater_pi5160000_altimages_ff_5160338-63dde443a49da303cb9dalt1_full.jpg?_hv=2&w=900'},
     {'id': 3, 'name': 'Accesories', 'description': 'Drinks Tumbler', 'price': 19.99, 'url': 'https://images.footballfanatics.com/georgetown-hoyas/georgetown-hoyas-18oz-stainless-steel-soft-touch-tumbler_pi4251000_ff_4251018-513e88aca198a4c5f3a2_full.jpg?_hv=2&w=900'},
-   # {'id': 4, 'name': 'Online Course', 'description': 'Level up your programming skills.', 'price': 129.99, 'url': 'https://picsum.photos/id/180/360/200'},
+    #{'id': 4, 'name': 'Online Course', 'description': 'Level up your programming skills.', 'price': 129.99, 'url': 'https://picsum.photos/id/180/360/200'},
     #{'id': 5, 'name': '________', 'description': '___________.', 'price': 0.99, 'url': '__________'},
     #{'id': 6, 'name': '________', 'description': '___________.', 'price': 0.99, 'url': '__________'},
 ]
